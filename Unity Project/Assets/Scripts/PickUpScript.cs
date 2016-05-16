d396a19056861a393de268245ef2365a049fc88a
12Gage <glhabiger@mymail.tstc.edu>
2016-05-16 10:59:32 -0500

    Unity

---
+++ Unity Project/Assets/Scripts/PickUpScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PickUpScript : MonoBehaviour {
+
+	public string itemName;
+
+	public GameObject InventoryHUD;
+
+	public Sprite Clover, Moss, Basil, Salt, Flower;
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.tag == "Player") {
+
+			if (itemName == "Clover") {
+
+				InventoryHUD.GetComponent<Image> ().sprite = Clover;
+
+				Destroy (gameObject);
+			}
+
+			if (itemName == "Moss") {
+
+				InventoryHUD.GetComponent<Image> ().sprite = Moss;
+
+				Destroy (gameObject);
+			}
+
+			if (itemName == "Basil") {
+
+				InventoryHUD.GetComponent<Image> ().sprite = Basil;
+
+				Destroy (gameObject);
+			}
+
+			if (itemName == "Salt") {
+
+				InventoryHUD.GetComponent<Image> ().sprite = Salt;
+
+				Destroy (gameObject);
+			}
+
+			if (itemName == "Flower") {
+
+				InventoryHUD.GetComponent<Image> ().sprite = Flower;
+
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int Health;
+
+	public float MaxHealth = 100.0f;
+
+	public Texture2D FullHealth, NoHealth;
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.tag.Contains ("Enemy")) {
+
+			Health -= 5;
+
+		}
+	}
+
+	void OnGUI(){
+
+		GUI.DrawTexture (new Rect (385, 40, NoHealth.width, NoHealth.height), NoHealth);
+
+		GUI.DrawTexture (new Rect (385, 40, FullHealth.width * (Health/MaxHealth), FullHealth.height), FullHealth);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
