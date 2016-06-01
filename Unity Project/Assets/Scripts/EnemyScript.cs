04d0388fa1a00b6da754ef0d614066b905c86732
12Gage <glhabiger@mymail.tstc.edu>
2016-06-01 10:56:17 -0500

    unity

--- Unity Project/Assets/Scripts/EnemyScript.cs
+++ Unity Project/Assets/Scripts/EnemyScript.cs
@@ -3,7 +3,19 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
 
-	public float Health = 5;
+	public Transform target;
+
+	Transform playerPos;
+
+	float playerDistance;
+
+	public float enemySpeed = 1.0f;
+
+	public int minHealth, maxHealth;
+
+	public int Health;
+
+	public float attackDist;
 
 	void OnTriggerEnter(Collider other){
 
@@ -20,11 +32,29 @@ public class EnemyScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		target = GameObject.FindWithTag ("Player").transform;
+
+		playerPos = GameObject.FindWithTag ("Player").transform;
+
+		Health = Random.Range (minHealth, maxHealth);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		playerDistance = Vector3.Distance (transform.position, playerPos.position);
+
+		if (playerDistance <= attackDist) {
+
+			transform.LookAt (target);
+
+			CharacterController controller = GetComponent<CharacterController> ();
+
+			Vector3 forward = transform.TransformDirection (Vector3.forward);
+
+			controller.SimpleMove (forward * enemySpeed);
+		}
 	}
 }
